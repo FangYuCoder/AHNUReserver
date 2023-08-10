0c98e9bb532d7631cdae42b491f0982d5cf0e109
15755843770 <15755843770@163.com>
2023-08-10 22:19:54 +0800

    changed some files

--- GetAndSend.py
+++ GetAndSend.py
@@ -23,88 +23,115 @@ headers = {
 
 class GetSend():
     def getSentence(self):
-        with open('./infos/名人名言.json', 'r', encoding='utf-8') as f:
-            sentences = json.load(f)
-        ran_sentence = random.randint(0, len(sentences) - 1)
-        sentence = sentences[ran_sentence]
-        return '每日名言：' + sentence
+        try:
+            with open('./infos/EnglishContent.json', 'r', encoding='utf-8') as f:
+                sentences = json.load(f)
+            ran_sentence = random.randint(0, len(sentences) - 1)
+            sentence = sentences[ran_sentence]
+            return '每日名言：' + sentence
+        except:
+            return '每日名言获取失败'
 
     def getHistory(self):
-        time.sleep(1)
-        history_url = ' https://www.mxnzp.com/api/history/today'  # 接口地址
-        response = requests.get(history_url, headers=headers)
-        res_data = response.json()  # 解析返回的 JSON 数据
-        if res_data['code'] == 1:  # 判断请求是否成功，具体根据返回数据格式进行调整
-            text = res_data['data'][0]
-            content = f'历史上的今天：\n{text["year"]} 年 {text["month"]} 月 {text["day"]} 日 {text["title"]}。'
-            return content
+        try:
+            time.sleep(1)
+            history_url = ' https://www.mxnzp.com/api/history/today'  # 接口地址
+            response = requests.get(history_url, headers=headers)
+            res_data = response.json()  # 解析返回的 JSON 数据
+            if res_data['code'] == 1:  # 判断请求是否成功，具体根据返回数据格式进行调整
+                text = res_data['data'][0]
+                content = f'历史上的今天：\n{text["year"]} 年 {text["month"]} 月 {text["day"]} 日 {text["title"]}。'
+                return content
+        except:
+            return '历史上的今天获取失败'
 
     def getWeather(self, city):
-        time.sleep(1)
-        weather_url = f' https://www.mxnzp.com/api/weather/current/{city}'
-        response = requests.get(weather_url, headers=headers)
-        res_data = response.json()
-        if res_data['code'] == 1:
-            mes = res_data['data']
-            content = f'{mes["address"]} 当前气温{mes["temp"]} 天气{mes["weather"]} ' \
-                      f'{mes["windDirection"]}风{mes["windPower"]} 空气湿度{mes["humidity"]}'
-            return content
+        try:
+            time.sleep(1)
+            weather_url = f' https://www.mxnzp.com/api/weather/current/{city}'
+            response = requests.get(weather_url, headers=headers)
+            res_data = response.json()
+            if res_data['code'] == 1:
+                mes = res_data['data']
+                content = f'{mes["address"]} 当前气温{mes["temp"]} 天气{mes["weather"]} ' \
+                          f'{mes["windDirection"]}风{mes["windPower"]} 空气湿度{mes["humidity"]}'
+                return content
+        except:
+            return '今日天气获取失败'
 
     def getCalendar(self):
-        time.sleep(1)
-        today = datetime.date.today()
-        formatted_date = today.strftime("%Y%m%d")
-        weather_url = f' https://www.mxnzp.com/api/holiday/single/{formatted_date}'
-        response = requests.get(weather_url, headers=headers)
-        res_data = response.json()
-        if res_data['code'] == 1:
-            mes = res_data['data']
-            content = f'今天是{mes["yearTips"]}年 {mes["lunarCalendar"]} \n忌：{mes["avoid"]}\n宜：{mes["suit"]}\n' \
-                      f'阳历今年第{mes["dayOfYear"]}天，第{mes["weekOfYear"]}周'
-            return content
+        try:
+            time.sleep(1)
+            today = datetime.date.today()
+            formatted_date = today.strftime("%Y%m%d")
+            weather_url = f' https://www.mxnzp.com/api/holiday/single/{formatted_date}'
+            response = requests.get(weather_url, headers=headers)
+            res_data = response.json()
+            if res_data['code'] == 1:
+                mes = res_data['data']
+                content = f'今天是{mes["yearTips"]}年 {mes["lunarCalendar"]} \n忌：{mes["avoid"]}\n宜：{mes["suit"]}\n' \
+                          f'阳历今年第{mes["dayOfYear"]}天，第{mes["weekOfYear"]}周'
+                return content
+        except:
+            return '今日万年历获取失败'
 
     def getNews(self):
-        time.sleep(1)
-        params = {"typeId": 532, "page": 1}
-        content = '每日新闻：'
-        newslist_url = f' https://www.mxnzp.com/api/news/list/v2'
-        response = requests.get(newslist_url, params=params, headers=headers)
-        res_data = response.json()
-        # 新闻返回成功
-        if res_data['code'] == 1:
-            # 随机挑选一个新闻
-            rand = random.randint(0, len(res_data['data']) - 1)
-            choosed = res_data["data"][rand]
-            # 提取出新闻的标题、来源和实践
-            content += choosed["title"] + '\n' + choosed["source"] + ' ' + choosed["postTime"] + '\n'
-            newsid = choosed["newsId"]
-            # 延迟一秒再发送
+        try:
             time.sleep(1)
-            news_url = f'https://www.mxnzp.com/api/news/details/v2'
-            response = requests.get(news_url, params={"newsId": newsid}, headers=headers)
-            detail_data = response.json()
-            if detail_data['code'] == 1:
-                for item in detail_data['data']["items"]:
-                    if item["type"] == "text":
-                        content += item["content"] + '\n'
-            return content
+
+            newslist_url = f' https://www.mxnzp.com/api/news/types/v2'
+            response = requests.get(newslist_url, headers=headers)
+            newslist = response.json()
+            if newslist["code"] == 1:
+                chooseType = random.randint(0, len(newslist['data'])-1)
+                typeid = newslist["data"][chooseType]['typeId']
+
+                time.sleep(1)
+                params = {"typeId": typeid, "page": 1}
+                content = '每日新闻：'
+                newslist_url = f' https://www.mxnzp.com/api/news/list/v2'
+                response = requests.get(newslist_url, params=params, headers=headers)
+                res_data = response.json()
+                # 新闻返回成功
+                if res_data['code'] == 1:
+                    # 随机挑选一个新闻
+                    rand = random.randint(0, len(res_data['data']) - 1)
+                    choosed = res_data["data"][rand]
+                    # 提取出新闻的标题、来源和实践
+                    content += choosed["title"] + '\n\n' + choosed["source"] + ' ' + choosed["postTime"] + '\n'
+                    newsid = choosed["newsId"]
+
+                    # 延迟一秒再发送
+                    time.sleep(1)
+                    news_url = f'https://www.mxnzp.com/api/news/details/v2'
+                    response = requests.get(news_url, params={"newsId": newsid}, headers=headers)
+                    detail_data = response.json()
+                    if detail_data['code'] == 1:
+                        for item in detail_data['data']["items"]:
+                            if item["type"] == "text":
+                                content += item["content"] + '\n'
+                return content
+        except:
+            return '每日新闻获取失败'
 
     def summary(self, uids=our_uids):
-        sentence = self.getSentence()  # 获取每日一句
-        history = self.getHistory()  # 获取历史上的今天
-
-        # 获取今日天气
-        weather = self.getWeather('芜湖市')
-        if len(uids) == 2:
-            weather2 = self.getWeather('翁源')
-            weather = weather + '\n' + weather2
-        # 获取万年历
-        calendar = self.getCalendar()
-        # 获取今日新闻
-        news = self.getNews()
-        # 汇总
-        content = f'{sentence}\n\n{calendar} \n\n{weather}\n\n{history}\n\n{news}'
-        return content
+        try:
+            sentence = self.getSentence()  # 获取每日一句
+            history = self.getHistory()  # 获取历史上的今天
+            # 获取今日天气
+            weather = self.getWeather('芜湖市')
+            if len(uids) == 2:
+                weather2 = self.getWeather('翁源')
+                weather = weather + '\n' + weather2
+            # 获取万年历
+            calendar = self.getCalendar()
+            # 获取今日新闻
+            news = self.getNews()
+            # 汇总
+            content = f'{sentence}\n\n{calendar} \n\n{weather}\n\n{history}\n\n{news}'
+            return content
+        except:
+            return '今日推送获取失败'
 
     def msgPush(self):
         content = self.summary()
--- Reserve.py
+++ Reserve.py
@@ -7,15 +7,15 @@ import random
 import time
 from wxpusher import WxPusher
 from GetAndSend import GetSend
+# wxpusher微信推送程序
+appToken = "AT_1YFc0dX3P53PKGOsUua6hf5b59ETYdDL"
 
 TOMORROW = str(datetime.date.today() + datetime.timedelta(days=1))
-fileloc = './log.txt'
+fileloc = './infos/log.txt'
 client_path = "./clients"
 
 logging.basicConfig(filename=fileloc, level=logging.DEBUG,
                     format=' %(asctime)s - %(levelname)s- %(message)s')
-# wxpusher微信推送程序
-appToken = ""
 # 请求头
 header = {
     # 设定报文头
@@ -38,6 +38,7 @@ def randSleep(least, most):
     random.seed(time.time())
     second = random.randint(least, most)
     time.sleep(second)
+    logging.info(f'Delayed by {second} second randomly!')
 
 
 def getClients(client_path):
@@ -45,27 +46,28 @@ def getClients(client_path):
     # 获取文件夹中的所有文件
     json_files = os.listdir(client_path)
     for json_name in json_files:
-        with open(os.path.join(client_path, json_name)) as f:
-            clients.append(json.load(f))
+        if os.path.splitext(json_name)[1] == '.json':
+            with open(os.path.join(client_path, json_name)) as f:
+                clients.append(json.load(f))
     return clients
 
 
 class Reserve:
     def __init__(self, **kwargs):
         self.info = kwargs
-        self.sid = 35
+        self.sid = 35  # 将座位号对应的sid定义到类内
         self.session = requests.Session()
 
     def reserve(self):
         self.login()
         try:
-            self.sid, whichone = self.convert(self.info['sid'])
+            self.sid = self.convert(self.info['sid'])
             # 记录对应规则
-            self.info['whichone'] = whichone
-            if whichone == 0:
-                logging.info("请输入正确的座位，只能预约2,3,4楼的自习室")
+            # self.info['whichone'] = whichone
+            # if whichone == 0:
+            #     logging.info("Please enter a valid seat, you can only book study rooms on the 2nd, 3rd, and 4th floors.")
             # 开始预约
-            logging.info('开始预约...')
+            logging.info('Begin to reserve...')
             header = {
                 # 设定报文头
                 'Host': 'libzwxt.ahnu.edu.cn',
@@ -89,12 +91,11 @@ class Reserve:
             reserve = self.session.post(reserveUrl, data=json.dumps(reserverData), headers=header)
             if '成功' in reserve.text:
                 logging.info(reserve.text)
-                logging.info('预约成功！座位号是 {0}'.format(self.info['sid']))
+                logging.info('Booking successful! The seat number is {0}'.format(self.info['sid']))
                 winxin = WINXIN(self.info)
                 winxin.send()
 
             if '预约成功' not in reserve.text:
-                # 预约未成功，再次尝试
                 # 预约时间延时10到15秒
                 randSleep(10, 15)
                 reserve = self.session.post(reserveUrl, data=json.dumps(reserverData), headers=header)
@@ -105,7 +106,7 @@ class Reserve:
                 elif '冲突' or '重复' in reserve.text:
                     # 时间和其他人有冲突
                     logging.warning(reserve.text)
-                    logging.warning('座位冲突，预约失败！')
+                    logging.warning('Seat conflict, booking failed!')
                 elif '二次' in reserve.text:
                     logging.info(reserve.text)
                 elif '成功' in reserve.text:
@@ -113,17 +114,16 @@ class Reserve:
                     logging.info(reserve.text)
                     winxin = WINXIN(self.info)
                     winxin.send()
-                    # break
                 else:
                     logging.info(reserve.text)
-                    logging.error('未知原因，未预约成功，请检查日志及数据设置！！！')
+                    logging.error('Unknown reason, booking unsuccessful, please check the logs and data settings!!!')
 
         except BaseException as e:
             logging.error(e)
 
     def login(self):
         logging.info('''
-                    start  with 账号:{0}, 密码:{1}, 座位号:{2}. 从{3}到{4}.'''
+                    start  with account: {0}, password: {1}, seatid: {2}. From {3} to {4}.'''
                      .format(self.info['account'], self.info['password'], self.info['sid'], TOMORROW + self.info['st'],
                              TOMORROW + self.info['et']))
 
@@ -142,67 +142,54 @@ class Reserve:
         login = self.session.post(postUrl, data=postData)
 
         if '个人中心' in login.content.decode():
-            logging.info('登录成功！')
+            logging.info('Login successful!')
 
     @staticmethod
     def convert(seat_code):  # nsk3004
+        """
+        :param seat_code: str like "nsk3004"
+        :return: sid converted by seated
+        """
         sid = 0
-        # 记录是哪个规则映射的
-        whichone = 0
         if seat_code[:3] == 'nzr':
             sid = int(seat_code[3:]) + 437
-            whichone = 1
         elif seat_code[:3] == 'nsk' and seat_code[3] == '1':
             sid = int(seat_code[3:]) + 95
-            whichone = 2
         elif seat_code[:3] == 'nsk' and seat_code[3] == '3':
             sid = int(seat_code[3:]) - 2477
-            whichone = 3
         elif seat_code[:3] == 'nsk' and seat_code[3] == '2':
             sid = int(seat_code[3:]) - 1177
-            whichone = 4
         elif seat_code[:3] == 'nbz':
             sid = int(seat_code[3:])
-            whichone = 5
         elif seat_code[:3] == 'nbk':
             sid = int(seat_code[3:])
-            whichone = 6
         elif seat_code[:3] == 'ndz':
             sid = int(seat_code[3:]) + 2875
-            whichone = 7
         # 三楼公共阅览室东
         elif seat_code[:5] == 'ngg3e':
             number = int(seat_code[5:])
             if number < 89:
                 sid = number + 2433
-                whichone = 8
             else:
                 sid = number - 89 + 2682
-                whichone = 9
         elif seat_code[:5] == 'ngg3w':
             number = int(seat_code[5:])
             sid = number + 2521
-            whichone = 10
         elif seat_code[:5] == 'ngg4e':
             number = int(seat_code[5:])
             if number < 33:
                 sid = number + 2617
-                whichone = 11
             else:
                 sid = number - 33 + 2754
-                whichone = 12
         elif seat_code[:5] == 'ngg4w':
             number = int(seat_code[5:])
             if number < 33:
                 sid = number + 2649
-                whichone = 13
             elif 33 <= number <= 96:
                 sid = number - 33 + 2690
-                whichone = 14
             else:
                 sid = number - 97 + 3143
-                whichone = 15
-        return sid, whichone
+        return sid
 
 
 class WINXIN:
@@ -212,38 +199,54 @@ class WINXIN:
     def send(self):
         # 推送内容
         if self.INFO['account'] == '20112001120':
-            nickname = '丘~~~~~'
+            nickname = '丘~预约成功！'
         elif self.INFO['account'] == '20111304054':
-            nickname = '超超~~~~~~'
-        elif self.INFO['account'] == '20111701089':
-            nickname = '铮~~~~~'
+            nickname = '超超~预约成功！'
         else:
             nickname = self.INFO['account']
         # 座位号
         seatid = self.INFO['sid']
-        # 每日一句
-        daily = GetSend()
-        dailyContent = daily.summary()
 
-        content = f"{nickname}\n\n您明天的座位已经预约完成！\n预约座位编号：{seatid}\n预约时间：{self.INFO['st']}~{self.INFO['et']}\n\n{dailyContent}"
-        WxPusher.send_message(
-            content=content,
-            summary='预约座位信息',
-            uids=self.INFO['uids'],
-            token=appToken,
-            topic_ids=[]
-        )
-        logging.info('消息已发出')
+        try:
+            # 每日一句
+            daily = GetSend()
+            dailyContent = daily.summary(uids=self.INFO['uids'])
+            content = f"{nickname}\n\n您明天的座位已经预约完成！\n预约座位编号：{seatid}\n预约时间：{self.INFO['st']}~{self.INFO['et']}\n\n{dailyContent}"
+        except:
+            content = f"{nickname}\n\n您明天的座位已经预约完成！\n预约座位编号：{seatid}\n预约时间：{self.INFO['st']}~{self.INFO['et']}"
+            logging.info('Failed to retrieve daily push, please check the log and code for error details!\n')
+        try:
+            WxPusher.send_message(
+                content=content,
+                summary='预约座位信息',
+                uids=self.INFO['uids'],
+                token=appToken,
+                topic_ids=[]
+            )
+            logging.info('Appointment successful, message has been sent!\n')
+        except:
+            logging.info('Message sending failed, appToken may not be configured.')
 
 
 if __name__ == '__main__':
-    randSleep(180, 480)
+    try:
+        push1 = GetSend()
+        push1.msgPush()
+        logging.info('Daily push has been sent.')
+    except:
+        logging.error('Non-booking code section, daily push message error, reason unknown!')
+
+    # 预约部分
+    randSleep(120, 300)
     clients = getClients(client_path)
     try:
         for i in range(len(clients)):
-            randSleep(60, 120)
-            reserve = Reserve(**clients[i])
-            reserve.reserve()
-    finally:
-        push1 = GetSend()
-        push1.msgPush()
+            try:
+                randSleep(120, 180)
+                reserve = Reserve(**clients[i])
+                reserve.reserve()
+            except:
+                logging.info(f'Account {clients[i]["account"]} failed to reserve...reason unkown')
+        logging.info('Today\'s appointment booking is over.\n\n\n\n\n')
+    except:
+        logging.error('Appointment code execution failed, reason unknown.')
